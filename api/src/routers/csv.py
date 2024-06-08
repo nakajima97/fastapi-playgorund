7f84067687ffd5b832d3ffa40d98d965a7fa2ca9
Yudai Nakajima <kituneudon97@gmail.com>
2024-06-08 17:16:51 +0900

    #1 csvをdownloadさせるエンドポイントの作成

--- api/src/routers/csv.py
+++ api/src/routers/csv.py
@@ -1,7 +1,15 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response
 
 router = APIRouter()
 
 @router.get("/csv")
 async def get_csv():
-    return {"message": "Hello World"}
+    content = "id,name\n1,John\n2,Doe"
+    filename = "sample.csv"
+
+    headers = {
+        "Content-Type": "text/csv",
+        "Content-Disposition": f"attachment; filename={filename}",
+    }
+
+    return Response(content, headers=headers)

ad9071413842dde127013ed52daaaf93f5129b44
nakajima97 <kituneudon97@gmail.com>
2024-06-13 22:12:53 +0900

    #8 フォーマッターの実行

--- api/src/cruds/memo.py
+++ api/src/cruds/memo.py
@@ -4,10 +4,14 @@ from sqlalchemy.engine import Result
 
 from src.models.memo import memo as memo_model
 
+
 def fetch_memo(db: Session, limit: int = 10, offset: int = 0):
-    result: Result = db.execute(select(memo_model.id, memo_model.content).limit(limit).offset(offset))
+    result: Result = db.execute(
+        select(memo_model.id, memo_model.content).limit(limit).offset(offset)
+    )
     rows = result.all()
     return [{"id": row[0], "content": row[1]} for row in rows]
+
 
 def count_memos(db: Session):
     result: Result = db.execute(select(func.count(memo_model.id)))
--- api/src/migrate_db.py
+++ api/src/migrate_db.py
@@ -7,9 +7,11 @@ DB_URL = "mysql+pymysql://root@mysql:3306/mydb?charset=utf8"
 # echo=True：SQLをログに書く
 engine = create_engine(DB_URL, echo=True)
 
+
 def reset_database():
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+
 
 if __name__ == "__main__":
     reset_database()

d85f1708511f1ae7c6f1caf5c007ab7d33a24b91
nakajima97 <kituneudon97@gmail.com>
2024-06-13 09:33:21 +0900

    #6 総ページ数も返すようにした

--- api/src/cruds/memo.py
+++ api/src/cruds/memo.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select, func
 from sqlalchemy.orm import Session
 from sqlalchemy.engine import Result
 
@@ -8,3 +8,8 @@ def fetch_memo(db: Session, limit: int = 10, offset: int = 0):
     result: Result = db.execute(select(memo_model.id, memo_model.content).limit(limit).offset(offset))
     rows = result.all()
     return [{"id": row[0], "content": row[1]} for row in rows]
+
+def count_memos(db: Session):
+    result: Result = db.execute(select(func.count(memo_model.id)))
+    count = result.scalar()
+    return count
--- api/src/routers/memo.py
+++ api/src/routers/memo.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from src.db import get_db
-from src.cruds.memo import fetch_memo
+from src.cruds.memo import fetch_memo, count_memos
 
 router = APIRouter()
 
 @router.get("/memo")
 async def get_memo(limit: int = 10, offset:int = 0, db: Session = Depends(get_db)):
-  return fetch_memo(db, limit=limit, offset=offset)
+  data = fetch_memo(db, limit=limit, offset=offset)
+  count = count_memos(db)
+  return {'data': data, 'count': count}

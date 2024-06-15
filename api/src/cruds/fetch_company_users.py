eb4525cd2516f2ff78c827d4c2c837051e6d9e87
nakajima97 <kituneudon97@gmail.com>
2024-06-15 12:52:47 +0900

    #14 lintとformatの注意点修正

--- api/src/cruds/fetch_company_users.py
+++ api/src/cruds/fetch_company_users.py
@@ -1,10 +1,18 @@
 from sqlalchemy.orm import Session, joinedload
 
 from src.models.company import Company
-from src.models.user import User
+
 
 def fetch_company_users_with_joinedload(db: Session, company_id: int):
-    return db.query(Company).options(joinedload(Company.users)).filter(Company.id == company_id).all()
+    return (
+        db.query(Company)
+        .options(joinedload(Company.users))
+        .filter(Company.id == company_id)
+        .all()
+    )
+
 
 def fetch_company_users_with_join(db: Session, company_id: int):
-    return db.query(Company).join(Company.users).filter(Company.id == company_id).first()
+    return (
+        db.query(Company).join(Company.users).filter(Company.id == company_id).first()
+    )

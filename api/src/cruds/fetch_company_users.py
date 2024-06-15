9492c3bd6356250b727176850b83e8613359f4da
nakajima97 <kituneudon97@gmail.com>
2024-06-15 11:59:56 +0900

    #14 単純にjoinで結合した際のエンドポイント作成

--- api/src/cruds/fetch_company_users.py
+++ api/src/cruds/fetch_company_users.py
@@ -3,3 +3,6 @@ from src.models.company import Company
 
 def fetch_company_users_with_joinedload(db: Session, company_id: int):
     return db.query(Company).options(joinedload(Company.users)).filter(Company.id == company_id).all()
+
+def fetch_company_users_with_join(db: Session, company_id: int):
+    return db.query(Company).join(Company.users).filter(Company.id == company_id).all()
--- api/src/routers/users.py
+++ api/src/routers/users.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
-from src.cruds.fetch_company_users import fetch_company_users_with_joinedload
+from src.cruds.fetch_company_users import fetch_company_users_with_joinedload, fetch_company_users_with_join
 from src.db import get_db
 
 router = APIRouter()
@@ -13,3 +13,7 @@ async def get_company_users(db: Session = Depends(get_db), company_id: int = 1):
     users = fetch_company_users_with_joinedload(db, company_id)
     return {"data": users}
 
+@router.get('/company/users/join', tags=[company_user_tag], description="joinを使って会社のユーザーを取得します。")
+async def get_company_users(db: Session = Depends(get_db), company_id: int = 1):
+    users = fetch_company_users_with_selectinload(db, company_id)
+    return {"data": users}

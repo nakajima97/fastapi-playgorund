ff5d1b1628774b1ca0bdfcc74ba0c515dcdb02ab
nakajima97 <kituneudon97@gmail.com>
2024-06-16 15:21:18 +0900

    #19 日付でフィルターをかける

--- api/src/cruds/fetch_company_memos.py
+++ api/src/cruds/fetch_company_memos.py
@@ -1,7 +1,9 @@
-from sqlalchemy.orm import Session, joinedload
+from sqlalchemy.orm import Session, joinedload, contains_eager
+from datetime import datetime
 
 from src.models.company import Company as company_model
 from src.models.user import User as user_model
+from src.models.memo import Memo as memo_model
 
 
 def fetch_company_memos_with_joinedload(db: Session, company_id: int):
@@ -22,6 +24,24 @@ def fetch_company_memos_with_join(db: Session, company_id: int):
     """
 
     query = db.query(company_model).join(company_model.users).join(user_model.memos).filter(company_model.id == company_id)
+
+    company = query.all()
+
+    return company
+
+def fetch_company_memos_with_join_filter(db: Session, company_id: int, start_date: datetime = None, end_date: datetime = None):
+    """joinを使ってメモの一覧を取得する関数
+    """
+
+    query = db.query(company_model).join(company_model.users).join(user_model.memos)\
+        .options(contains_eager(company_model.users).contains_eager(user_model.memos))\
+        .filter(company_model.id == company_id)
+
+    if start_date:
+        query = query.filter(memo_model.created_at >= start_date)
+
+    if end_date:
+        query = query.filter(memo_model.created_at <= end_date)
 
     company = query.all()
 
--- api/src/routers/memo.py
+++ api/src/routers/memo.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from datetime import datetime
 
 from src.db import get_db
 from src.cruds.memo import fetch_memo, count_memos
-from src.cruds.fetch_company_memos import fetch_company_memos_with_joinedload, fetch_company_memos_with_join
+from src.cruds.fetch_company_memos import fetch_company_memos_with_joinedload, fetch_company_memos_with_join, fetch_company_memos_with_join_filter
 
 router = APIRouter()
 
@@ -34,6 +35,25 @@ async def get_company_memo_with_joinedload(company_id: int = 1, db: Session = De
 )
 async def get_company_memo_with_joinedload(company_id: int = 1, db: Session = Depends(get_db)):
     company = fetch_company_memos_with_join(db, company_id)
+
+    users = company[0].users
+    company_dict = company[0].__dict__
+    company_dict["users"] = []
+
+    for user in users:
+        user_dict = user.__dict__
+        user_dict["memos"] = user.memos
+        company_dict["users"].append(user_dict)
+
+    return {"data": [company_dict]}
+
+@router.get(
+    "/company/{company_id}/memo/join/filter",
+    tags=[company_memo_tag],
+    name="joinを使ってmemoを取得。日付で絞り込みが可能。"
+)
+async def get_company_memo_with_joinedload(start_date: datetime = None, end_date: datetime = None, company_id: int = 1, db: Session = Depends(get_db)):
+    company = fetch_company_memos_with_join_filter(db, company_id, start_date, end_date)
 
     users = company[0].users
     company_dict = company[0].__dict__
